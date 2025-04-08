824d935eaf640788b17148851de0359e843c2a1c
REKHAVSP <89581747+REKHAVSP@users.noreply.github.com>
2025-04-08 10:22:04 +0530

    App.js

--- frontend_app/src/App.js
+++ frontend_app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hey Gorgeous!!! Welcome  !!
+          Hey Gorgeous!!! Welcome!!
         </p>
         <a
           className="App-link"
