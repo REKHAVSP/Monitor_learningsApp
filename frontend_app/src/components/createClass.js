174867bc4baba434c0c89a347d572942cb1ac7e8
REKHAVSP <89581747+REKHAVSP@users.noreply.github.com>
2025-04-09 16:35:10 +0530

    react class nd fns

---
+++ frontend_app/src/components/createClass.js
@@ -0,0 +1,11 @@
+import React from "react";
+class ClassCreation extends React.Component{
+    render() {
+        const class_name ="sending something"
+        return (
+            <h2>HEY ITS A CLASS </h2>
+        );
+    }
+}
+
+export default ClassCreation;
---
+++ frontend_app/src/components/createFunctions.js
@@ -0,0 +1,15 @@
+
+import ClassCreation from "./createClass"
+
+
+
+function CreateFunctions() {
+    const show ="Im have used a ClassCreation "
+    const name ="asigned a variable to it "
+  return (
+    <><h1>HEY {show} nd {name}!!!</h1><ClassCreation /></>
+  );
+}
+
+
+export default CreateFunctions;
