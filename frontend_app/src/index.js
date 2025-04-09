365423066039011d1f12b7665c2d9fd0f60059e1
REKHAVSP <89581747+REKHAVSP@users.noreply.github.com>
2025-04-09 16:36:42 +0530

    calling new fn

--- frontend_app/src/index.js
+++ frontend_app/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import CreateFunctions from './components/createFunctions'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <CreateFunctions />
   </React.StrictMode>
 );
 
